7da22231fc498adfdca2c3f1adb98dbc76e445dc
Matthias Zipp <matthias.zipp@hadiko.de>
2019-10-28 14:35:45 +0100

    changed network: 1st conv kernel is also affected by "kernel_size", dropout is now normalized i.e. divided by fc_layer_count

--- custom_train.py
+++ custom_train.py
@@ -4,23 +4,23 @@ import init_data
 if __name__ == '__main__':
 
 	custom_genes = [0.0001,  # learning rate
-					2,  # feature_size
-					2,  # conv_layer_count
+					50,  # feature_size
+					5,  # conv_layer_count
 					2,  # fc_layer_count
-					2,  # kernel_size
-					1,  # dilation_rate
+					5,  # kernel_size
+					20,  # dilation_rate
 					0.1]  # dropout
 
 	data_config = {"train_data_dir": "data/training_data",
-						"test_data_dir": "data/test_data",
-						"train_cut_start": 0,
-						"train_cut_length": 6000,
-						"test_cut_start": 1000,
-						"test_cut_length": 5000,
-						"aug_multiplier": 10}
+					"test_data_dir": "data/test_data",
+					"train_cut_start": 0,
+					"train_cut_length": 6000,
+					"test_cut_start": 1000,
+					"test_cut_length": 5000,
+					"aug_multiplier": 5}
 
 	train_test_config = {"train_epochs": 1000,
-						 "train_batch_size": 100,
+						 "train_batch_size": 200,
 						 "test_batch_size": 48,
 						 "log_file_path": "run_log.txt",
 						 "fold_count": 5,
--- dynamic_net.py
+++ dynamic_net.py
@@ -7,7 +7,7 @@ from typing import Dict
 def dynamic_net(params: Dict):
     model = Sequential()
 
-    model.add(Conv1D(params['feature_size'], 1, activation='relu', input_shape=(5000, 8)))
+    model.add(Conv1D(params['feature_size'], params['kernel_size'], activation='relu', input_shape=(5000, 8)))
     for i in range(0, params['conv_layer_count']):
         model.add(Conv1D(params['feature_size'], params['kernel_size'], dilation_rate=params['dilation_rate'], activation='relu'))
 
@@ -15,10 +15,10 @@ def dynamic_net(params: Dict):
 
     for i in range(0, params['fc_layer_count']):
         model.add(Dense(256, activation='relu'))
-        model.add(Dropout(params['dropout']))
+        model.add(Dropout(params['dropout']/params['fc_layer_count']))  # normalize dropout for multiple layers
 
     model.add(Dense(3, activation='softmax'))
 
-    adam = Adam(lr=params['lr'], beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    adam = Adam(lr=params['lr'], beta_1=0.9, beta_2=0.999, decay=0.0, amsgrad=False)
     model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
     return model
